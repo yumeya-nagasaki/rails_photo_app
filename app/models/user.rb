e888583e7ff84234d21b5b0f535a7e9068c929fa
Setsura Kawabata <kawabata@yumeya.nagasaki.jp>
2026-06-28 13:58:15 +0900

    feature: ログイン機能の作成

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  has_secure_password
+  has_many :sessions, dependent: :destroy
+
+  normalizes :email_address, with: ->(e) { e.strip.downcase }
+
+  validates :email_address, presence: true, uniqueness: true
+  validates :password, length: { minimum: 8 }, allow_nil: true
+end
